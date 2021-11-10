55a5dcb2b2e208a5f61a67035825d741a6f2815c
siemens-c2c <joy_mon78@yahoo.co.in>
2021-11-10 11:21:13 +0530

    day-1 1st upload

---
+++ codes/day-1/FirstDemo/FirstDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FirstDemo
+{
+    class Program
+    {
+        //1. static void Main(string[] args)
+        //2. static int Main()
+        //3. static int Main(string[] args)
+        static void Main()
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
