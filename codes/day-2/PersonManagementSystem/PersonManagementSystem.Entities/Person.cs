b2fa155a8e00b340e6768602cad7484bcca37154
siemens-c2c <joy_mon78@yahoo.co.in>
2021-11-11 13:14:45 +0530

    day-2 1st upload

---
+++ codes/day-2/PersonManagementSystem/PersonManagementSystem.Entities/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace PersonManagementSystem.Entities
+{
+    public class Person
+    {
+        //string _NameFiled;
+        //string _EmailField;
+        //long _MobileNumnerField;
+        //string _LocationField;
+
+        #region Constructors
+        public Person()
+        {
+
+        }
+
+        public Person(string personName, string email, long mobile, string place)
+        {
+            this.Name = Name;
+            this.MobileNumber = mobile;
+            this.Location = place;
+            this.EmailId = email;
+        }
+        #endregion
+
+        #region Auto-implemented Properties
+        public string Name
+        {
+            set;
+            get;
+        }
+
+        /**
+         *  public string Name
+        {
+            set =>_NameField = value;
+            get => _NameFiled;
+        }
+         */
+        public long MobileNumber
+        {
+            get;
+            set;
+        }
+
+        public string EmailId
+        {
+            get;
+            set;
+        }
+
+        public string Location
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Methods
+        public string GetInformation()
+        {
+            return this.Name + " " + this.Location;
+        }
+        #endregion
+    }
+}
---
+++ codes/day-2/PersonManagementSystem/PersonManagementSystem.UserInterface/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using PersonManagementSystem.Entities;
+
+namespace PersonManagementSystem.UserInterface
+{
+    class Program
+    {
+        static void Main()
+        {
+            Person arunPersonObjRef = null;
+            arunPersonObjRef = new Person("arun", "arun@gmail.com", 9090909090, "hyderabad");
+
+            Person sakshamPersonObjRef = new Person("saksham", "saksham@gmail.com", 9090909091, "agra");
+            string informationOfArun = arunPersonObjRef.GetInformation();
+            Console.WriteLine(informationOfArun);
+            string informationOfSaksham = sakshamPersonObjRef.GetInformation();
+            Console.WriteLine(informationOfSaksham);
+
+            //Console.WriteLine(sakshamPersonObjRef.GetMobileNumber());
+            Console.WriteLine(sakshamPersonObjRef.MobileNumber);
+
+            //sakshamPersonObjRef.SetMobileNumber(9090919293);
+            //sakshamPersonObjRef.MobileNumber = 9090919293;
+
+            //Console.WriteLine(sakshamPersonObjRef.GetMobileNumber());
+            Console.WriteLine(sakshamPersonObjRef.MobileNumber);
+
+            Person vinayaPersonObjRef = new Person();
+            vinayaPersonObjRef.Name = "Vinaya P V";
+            vinayaPersonObjRef.EmailId = "vinaya@gmail.com";
+            vinayaPersonObjRef.MobileNumber = 9191919191;
+            vinayaPersonObjRef.Location = "Chennai";
+
+            Console.WriteLine(vinayaPersonObjRef.Name);
+
+            //Person vimalPersonObjRef = new Person("Vimal Belawal", "vimal@gmail.com", 9292929292, "Mumbai");
+            Person vimalPersonObjRef = new Person
+            {
+                Name = "Vimal Belawal",
+                EmailId = "vimal@gmail.com",
+                MobileNumber = 9292929292,
+                Location = "Mumbai"
+            };
+        }
+    }
+}
