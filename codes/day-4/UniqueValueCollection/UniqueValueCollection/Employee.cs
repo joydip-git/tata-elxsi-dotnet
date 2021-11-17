d441da3788e30cb756d2be6fdb39c655c672753b
siemens-c2c <joy_mon78@yahoo.co.in>
2021-11-17 16:38:51 +0530

    Day-4 6th upload

---
+++ codes/day-4/UniqueValueCollection/UniqueValueCollection/Employee.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueValueCollection
+{
+    class Employee : IComparable<Employee>
+    {
+        #region Data Members
+        private int _id;
+        private string _name;
+        private decimal _basicPay;
+        private decimal _daPay;
+        private decimal _hraPay;
+        private decimal _totalSalary;
+        #endregion
+
+        #region Constructor
+        public Employee() { }
+
+        public Employee(int id, string name, decimal basicPay, decimal daPay, decimal hraPay)
+        {
+            _id = id;
+            _name = name;
+            _basicPay = basicPay;
+            _daPay = daPay;
+            _hraPay = hraPay;
+        }
+        #endregion
+
+        #region Properties       
+
+        public decimal TotalSalary
+        {
+            get { return _totalSalary; }
+            protected set { _totalSalary = value; }
+        }
+
+        public decimal HraPay
+        {
+            get { return _hraPay; }
+            set { _hraPay = value; }
+        }
+
+        public decimal DaPay
+        {
+            get { return _daPay; }
+            set { _daPay = value; }
+        }
+
+        public decimal BasicPay
+        {
+            get { return _basicPay; }
+            set { _basicPay = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        #endregion
+
+        #region Methods
+        public void CalculateSalary()
+        {
+            this._totalSalary = this._basicPay + this._daPay + this._hraPay;
+        }
+        #endregion
+
+        #region Object Method Overrides
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                throw new NullReferenceException("reference is null");
+
+            if (!(obj is Employee))
+                throw new ArgumentException($"not of type {this.GetType().Name}");
+
+            Employee other = obj as Employee;
+            if (this == other)
+                return true;
+
+            if (!this._id.Equals(other._id))
+                return false;
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            const int random = 31;
+            return this._id.GetHashCode() * random;
+        }
+
+        public override string ToString()
+        {
+            return $"{this._name}, {this._totalSalary}";
+        }
+        #endregion
+
+        #region Optional: IComparable Interface Implementation
+        public int CompareTo(Employee other)
+        {
+            if (other == null)
+            {
+                throw new NullReferenceException("reference is null");
+            }
+            if (this == other)
+                return 0;
+
+            return this._id.CompareTo(other._id);
+        }
+        #endregion
+    }
+}
---
+++ codes/day-4/UniqueValueCollection/UniqueValueCollection/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueValueCollection
+{
+    class Student //:object //: IComparable<Student>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public override int GetHashCode()
+        {
+            const int seedValue = 23;
+            int hash = this.Id.GetHashCode() * seedValue;
+            return hash;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                throw new NullReferenceException("null has been passed");
+
+            if (!(obj is Student))
+            {
+                throw new ArgumentException("Not of type Student");
+            }
+
+            Student other = obj as Student;
+            if (this == other)
+                return true;
+
+            if (!this.Id.Equals(other.Id))
+                return false;
+
+            if (!this.Name.Equals(other.Name))
+                return false;
+
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Id}, {this.Name}";
+        }
+
+        /*
+        public int CompareTo(Student other)
+        {
+            if (other == null)
+                throw new NullReferenceException("other instance is NULL");
+
+            if (this == other)
+                return 0;
+
+            if (this.Id.CompareTo(other.Id) == 0)
+                return this.Name.CompareTo(other.Name);
+            else
+                return this.Id.CompareTo(other.Id);
+        }
+        */
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Student anilStudent = new Student { Id = 1, Name = "anil" };
+            Student sunilStudent = new Student { Id = 2, Name = "sunil" };
+            //Student sunilStudent = anilStudent;
+
+            //Console.WriteLine(anilStudent.ToString());
+            //Console.WriteLine(sunilStudent);
+
+            // if (anilStudent.Equals(sunilStudent))
+            //if (anilStudent.GetHashCode().Equals(sunilStudent.GetHashCode()))
+            //{
+            //    if (anilStudent.Equals(sunilStudent))
+            //        Console.WriteLine("same");
+            //    else
+            //        Console.WriteLine("not same");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("not same");
+            //}
+            //if (this == other)
+            //{
+            //    return true
+            //}
+            //else
+            //{
+            //   return false;
+            //}
+
+            /*
+            //comparison: greater, less and equal
+            anilStudent.CompareTo(sunilStudent);
+
+            List<Student> students = new List<Student>
+            {
+                anilStudent,sunilStudent
+            };
+            students.Sort();
+            */
+
+            //collection of unique items
+            //HashSet<int> numbers = new HashSet<int>();
+            //numbers.Add(12);
+            //numbers.Add(1);
+            //numbers.Add(12);
+            //numbers.Add(13);
+
+            //foreach (int item in numbers)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            HashSet<Student> students = new HashSet<Student>();
+            students.Add(anilStudent);
+            students.Add(sunilStudent);
+            students.Add(anilStudent);
+
+            foreach (Student item in students)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
